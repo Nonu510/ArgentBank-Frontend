7c54e34338f11016945d6e3ae98f9210f1465144
Nolwenn Dubourg <nold14@hotamil.fr>
2025-10-30 14:43:31 -0300

    création de la structure des pages ajout photos dans le dossier

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import Header from './components/header/header.jsx'
+
+// import Error from './pages/error/error.jsx'
+// import Login from './pages/login/login.jsx'
+// import Account from './pages/account/account.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Router>
+      <Header />
+            <Routes>
+                <Route path="/" element={<App />} />
+    
+            </Routes>
+
+    </Router>
+    
   </StrictMode>,
 )
