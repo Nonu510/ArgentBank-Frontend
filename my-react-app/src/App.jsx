3726d79f8b55af539bd831f528e65fae57fe1f7c
Nolwenn Dubourg <nold14@hotamil.fr>
2025-10-30 18:25:30 -0300

    ajout features et features item

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import Banner from './components/banner/banner'
+import Features from './components/features/features'
 
 function App() {
   
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Banner />
+      <Features />
     </>
   )
 }
--- my-react-app/src/components/features/features.jsx
+++ my-react-app/src/components/features/features.jsx
@@ -1,19 +1,46 @@
-import './features.css'
+import React from 'react';
+import FeatureItem from '../feature-item/feature-item';
+import './features.scss';
+import chatIcon from '../../assets/icon-chat.png';
+import moneyIcon from '../../assets/icon-money.png';
+import securityIcon from '../../assets/icon-security.png';
 
-function Banner() {
+const Features = () => {
+  const featureData = [
+    {
+      iconSrc: chatIcon,
+      iconAlt: "Chat Icon",
+      title: "You are our #1 priority",
+      description: "Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes."
+    },
+    {
+      iconSrc: moneyIcon,
+      iconAlt: "Money Icon",
+      title: "More savings means higher rates",
+      description: "The more you save with us, the higher your interest rate will be!"
+    },
+    {
+      iconSrc: securityIcon,
+      iconAlt: "Security Icon",
+      title: "Security you can trust",
+      description: "We use top of the line encryption to make sure your data and money is always safe."
+    }
+  ];
+
   return (
-    <div className="hero">
-        <section className="hero-content">
-          <h2 className="sr-only">Promoted Content</h2>
-          <p className="subtitle">No fees.</p>
-          <p className="subtitle">No minimum deposit.</p>
-          <p className="subtitle">High interest rates.</p>
-          <p className="text">Open a savings account with Argent Bank today!</p>
-        </section>
-    </div>
-  )
-}
+    <section className="features">
+      <h2 className="sr-only">Features</h2>
+      {featureData.map((feature, index) => (
+        <FeatureItem
+          key={index}
+          iconSrc={feature.iconSrc}
+          iconAlt={feature.iconAlt}
+          title={feature.title}
+          description={feature.description}
+        />
+      ))}
+    </section>
+  );
+};
 
-export default Banner
-
-
+export default Features;
